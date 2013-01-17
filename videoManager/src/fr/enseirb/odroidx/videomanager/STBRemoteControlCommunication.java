0ef64ba8d1e0c499717b12599d68c2f36f138a3c
sylvain <sylvain@mutex>
2013-01-17 19:02:06 +0100

    update STBRemoteControlCommunication

--- videoManager/src/fr/enseirb/odroidx/videomanager/STBRemoteControlCommunication.java
+++ videoManager/src/fr/enseirb/odroidx/videomanager/STBRemoteControlCommunication.java
@@ -22,19 +22,22 @@ public class STBRemoteControlCommunication {
     boolean mIsBound;
     final Messenger mMessenger = new Messenger(new IncomingHandler());
 
-	private static final int CMD__VIDEO_PLAY = 10;
-	private static final int CMD__VIDEO_PAUSE = 11;
-	private static final int CMD__VIDEO_PREVIOUS = 12;
-	private static final int CMD__VIDEO_REWIND = 13;
-	private static final int CMD__VIDEO_FORWARD = 14;
-	private static final int CMD__VIDEO_NEXT = 15;
-	private static final int CMD__MOVE_UP = 16;
-	private static final int CMD__MOVE_DOWN = 17;
-	private static final int CMD__MOVE_LEFT = 18;
-	private static final int CMD__MOVE_RIGHT = 19;
-	private static final int CMD__SELECT = 20;
-	private static final int CMD__HOME = 21;
-	private static final int CMD__BACK = 22;
+    public static final int CMD__VIDEO_PLAY = 10;
+	public static final int CMD__VIDEO_PAUSE = 11;
+	public static final int CMD__VIDEO_STOP = 23;
+	public static final int CMD__VIDEO_PREVIOUS = 12;
+	public static final int CMD__VIDEO_NEXT = 15;
+	public static final int CMD__MOVE_UP = 16;
+	public static final int CMD__MOVE_DOWN = 17;
+	public static final int CMD__MOVE_LEFT = 18;
+	public static final int CMD__MOVE_RIGHT = 19;
+	public static final int CMD__SELECT = 20;
+	public static final int CMD__HOME = 21;
+	public static final int CMD__BACK = 22;
+	public static final int CMD__SOUND_MUTE = 24;
+	public static final int CMD__SOUND_PLUS = 25;
+	public static final int CMD__SOUND_MINUS = 26;
+	public static final int CMD__USER_TEXT = 27;
     
 	private static final int MSG__REGISTER_CLIENT = 1;
 	private static final int MSG__UNREGISTER_CLIENT = 2;
@@ -59,11 +62,8 @@ public class STBRemoteControlCommunication {
 	            case CMD__VIDEO_PREVIOUS:
 	            	press_key(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
 	            	break;
-	            case CMD__VIDEO_REWIND:
-	            	press_key(KeyEvent.KEYCODE_MEDIA_REWIND);
-	            	break;
-	            case CMD__VIDEO_FORWARD:
-	            	press_key(KeyEvent.KEYCODE_MEDIA_FAST_FORWARD);
+	            case CMD__VIDEO_STOP:
+	            	press_key(KeyEvent.KEYCODE_MEDIA_STOP);
 	            	break;
 	            case CMD__VIDEO_NEXT:
 	            	press_key(KeyEvent.KEYCODE_MEDIA_NEXT);
@@ -87,8 +87,29 @@ public class STBRemoteControlCommunication {
 	            	press_key(KeyEvent.KEYCODE_BACK);
 	            	break;
 	            case CMD__HOME:
-	            	press_key(KeyEvent.KEYCODE_HOME);
+	            	//press_key(KeyEvent.KEYCODE_HOME);
+	            	try {
+		            	Intent launchAppIntent = act.getPackageManager().getLaunchIntentForPackage("fr.enseirb.odroidx.home");
+		            	act.startActivity(launchAppIntent);
+	            	} 
+	            	catch(Exception e) {
+	            		Log.e(TAG, "Cannot lanch Home activity");
+	            	}
 	            	break;
+				case CMD__SOUND_MUTE:
+					press_key(KeyEvent.KEYCODE_VOLUME_MUTE);
+					break;
+				case CMD__SOUND_PLUS:
+					press_key(KeyEvent.KEYCODE_VOLUME_UP);
+					break;
+				case CMD__SOUND_MINUS:
+					press_key(KeyEvent.KEYCODE_VOLUME_DOWN);
+					break;
+				case CMD__USER_TEXT:
+					String user_text = msg.getData().getString("msg_value");
+					Log.v(TAG, "receiving from the STB: "+user_text);
+					press_key_corresponding_to_string(user_text);
+					break;
 	            default:
 	                super.handleMessage(msg);
             }
@@ -101,7 +122,9 @@ public class STBRemoteControlCommunication {
     	    	try {
     	    		Instrumentation inst = new Instrumentation();
     		        inst.sendKeyDownUpSync(keyCmd);
-    		        Log.v(TAG, "key "+keyCmd+"pressed");
+    		        Log.v(TAG, "key "+keyCmd+" pressed");
+    	    	} catch (SecurityException e) {
+    	    		Log.e(TAG, "Injecting Keys to other application is impossible due to non-system-permission");
     	    	} catch (Exception e) {
     	    		Log.e(TAG, "error while simulating key event :\n", e);
     	    	}
@@ -109,6 +132,50 @@ public class STBRemoteControlCommunication {
     	};
     	myThread.start();
     }
+
+	private void press_key_corresponding_to_string (String msg) {
+		for (char ch: msg.toCharArray()) {
+			switch(ch) {
+				case '.': press_key(KeyEvent.KEYCODE_NUMPAD_DOT); break;
+				case '0': press_key(KeyEvent.KEYCODE_NUMPAD_0); break;
+				case '1': press_key(KeyEvent.KEYCODE_NUMPAD_1); break;
+				case '2': press_key(KeyEvent.KEYCODE_NUMPAD_2); break;
+				case '3': press_key(KeyEvent.KEYCODE_NUMPAD_3); break;
+				case '4': press_key(KeyEvent.KEYCODE_NUMPAD_4); break;
+				case '5': press_key(KeyEvent.KEYCODE_NUMPAD_5); break;
+				case '6': press_key(KeyEvent.KEYCODE_NUMPAD_6); break;
+				case '7': press_key(KeyEvent.KEYCODE_NUMPAD_7); break;
+				case '8': press_key(KeyEvent.KEYCODE_NUMPAD_8); break;
+				case '9': press_key(KeyEvent.KEYCODE_NUMPAD_9); break;
+				case 'A': press_key(KeyEvent.KEYCODE_A); break;
+				case 'B': press_key(KeyEvent.KEYCODE_B); break;
+				case 'C': press_key(KeyEvent.KEYCODE_C); break;
+				case 'D': press_key(KeyEvent.KEYCODE_D); break;
+				case 'E': press_key(KeyEvent.KEYCODE_E); break;
+				case 'F': press_key(KeyEvent.KEYCODE_F); break;
+				case 'G': press_key(KeyEvent.KEYCODE_G); break;
+				case 'H': press_key(KeyEvent.KEYCODE_H); break;
+				case 'I': press_key(KeyEvent.KEYCODE_I); break;
+				case 'J': press_key(KeyEvent.KEYCODE_J); break;
+				case 'K': press_key(KeyEvent.KEYCODE_K); break;
+				case 'L': press_key(KeyEvent.KEYCODE_L); break;
+				case 'M': press_key(KeyEvent.KEYCODE_M); break;
+				case 'N': press_key(KeyEvent.KEYCODE_N); break;
+				case 'O': press_key(KeyEvent.KEYCODE_O); break;
+				case 'P': press_key(KeyEvent.KEYCODE_P); break;
+				case 'Q': press_key(KeyEvent.KEYCODE_Q); break;
+				case 'R': press_key(KeyEvent.KEYCODE_R); break;
+				case 'S': press_key(KeyEvent.KEYCODE_S); break;
+				case 'T': press_key(KeyEvent.KEYCODE_T); break;
+				case 'U': press_key(KeyEvent.KEYCODE_U); break;
+				case 'V': press_key(KeyEvent.KEYCODE_V); break;
+				case 'W': press_key(KeyEvent.KEYCODE_W); break;
+				case 'X': press_key(KeyEvent.KEYCODE_X); break;
+				case 'Y': press_key(KeyEvent.KEYCODE_Y); break;
+				case 'Z': press_key(KeyEvent.KEYCODE_Z); break;
+			}
+		}
+	}
     
     private ServiceConnection mConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName className, IBinder service) {
